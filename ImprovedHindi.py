3613632b98da7fc971225378771b0d61ee25e9ed
Dru-O7 <e21cseu0291@bennett.edu.in>
2024-06-24 15:34:48 +0530

    improved Hindi model and uploaded joblib files

---
+++ ImprovedHindi.py
@@ -0,0 +1,20 @@
+import joblib
+
+classifier = joblib.load('svm_classifier.joblib')
+vectorizer = joblib.load('tfidf_vectorizer.joblib')
+
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word.isalpha()]
+    preprocessed_text = ' '.join(tokens)
+    return preprocessed_text
+
+def predict_sentiment(text):
+    preprocessed_text = preprocess_text(text)
+    text_tfidf = vectorizer.transform([preprocessed_text])
+    prediction = classifier.predict(text_tfidf)[0]
+    return prediction
+
+input_text = transcript.text
+predicted_sentiment = predict_sentiment(input_text)
+print(f"Predicted Sentiment: {predicted_sentiment}")
